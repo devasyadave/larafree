c6af6cf8263986bc48131893b3bc0f3021f76a44
devasyadave <devasyadave@gmail.com>
2019-04-15 15:41:43 +0530

    First .git push

---
+++ src/classes/actions/2014_10_12_100000_create_miniorange_tables.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMiniorangeTables extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mo_config', function (Blueprint $table) {
+            $table->string('id', 10)->unique();
+            $table->string('mo_saml_host_name', 100);
+            $table->string('mo_saml_admin_email', 100);
+            $table->string('mo_saml_admin_password', 100);
+            $table->string('mo_saml_admin_customer_key', 100);
+            $table->string('mo_saml_admin_api_key', 100);
+            $table->string('mo_saml_customer_token', 100);
+            $table->string('mo_saml_free_version', 100);
+            $table->string('mo_saml_message', 300);
+            $table->string('saml_identity_name', 100);
+            $table->string('idp_entity_id', 100);
+            $table->string('saml_login_url', 100);
+            $table->string('saml_login_binding_type', 100);
+            $table->string('sp_base_url', 100);
+            $table->string('sp_entity_id', 100);
+            $table->string('acs_url', 100);
+            $table->string('single_logout_url', 100);
+            $table->string('saml_am_email', 100);
+            $table->string('saml_am_username', 100);
+            $table->string('relaystate_url', 100);
+            $table->string('site_logout_url', 100);
+            $table->string('saml_x509_certificate', 1500);
+            $table->string('mo_saml_new_registration', 10)->default('true');
+            $table->string('mo_saml_admin_phone', 20);
+            $table->string('mo_saml_verify_customer', 10);
+            $table->string('mo_saml_idp_config_complete', 100);
+            $table->string('mo_saml_transactionId', 100);
+            $table->string('mo_saml_guest_enabled',10);
+            $table->string('mo_saml_registration_status', 100);
+            $table->string('session_index', 100);
+        });
+        Schema::create('mo_admin', function (Blueprint $table) {
+            $table->string('id', 10);
+            $table->string('email', 100);
+            $table->string('password', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mo_config');
+        Schema::dropIfExists('mo_admin');
+    }
+}
